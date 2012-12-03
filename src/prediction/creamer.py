b47492ca5d8ff246ca953f6e505d09fa0012add5
Ash Booth <ash.booth@soton.ac.uk>
2012-12-03 15:36:29 +0000

    More file structure shifting

---
+++ src/prediction/creamer.py
@@ -0,0 +1,17 @@
+'''
+Created on Dec 3, 2012
+
+@author: Ash Booth
+'''
+
+class Creamer(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
